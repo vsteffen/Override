b5a134717a5e4ee8fc87ecf5670fe983932b5f09
missmaelyss <marnaud@e2r2p17.42.fr>
2020-03-03 23:06:19 +0100

    level01

--- level01/source.c
+++ level01/source.c
@@ -1,37 +1,46 @@
-char	a_user_name[0x100];
+// gcc -fno-stack-protector -z execstack -m32
+
+#include <string.h>
+#include <stdio.h>
+
+char    a_user_name[100];
 
 int verify_user_pass(char *buff)
 {
-	return (memcmp(buff, "admin", 0x5));
+        return (strncmp(buff, "admin", 0x5));
 }
 
 int verify_user_name()
 {
-	puts("verifying username....\n");
-	return (memcmp(a_user_name, "dat_wil", 0x7));
+        puts("verifying username....\n");
+        return (strncmp(a_user_name, "dat_wil", 0x7));
 }
 
 int main() {
-	char buff[0x40];
-	int diff;
+        char buff[0x40];
+        int diff;
 
-	memset(buff, 0, 0x40);
-	puts("********* ADMIN LOGIN PROMPT *********");
-	printf("Enter Username: ");
-	fgets(a_user_name, 0x100, STDIN);
-	diff = verify_user_name();
-	if (diff != 0)
-	{
-		puts("nope, incorrect username...\n");
-		return (1);
-	}
-	puts("Enter Password: ");
-	fgets(buff, 0x64, STDIN);
-	diff = verify_user_pass(buff);
-	if (diff == 0)
-	{
-		puts("nope, incorrect password...\n");
-		return (1);
-	}
-	return (0);
+        memset(buff, 0, 0x40);
+        diff = 0;
+        puts("********* ADMIN LOGIN PROMPT *********");
+        printf("Enter Username: ");
+        fgets(a_user_name, 0x100, stdin);
+        diff = verify_user_name();
+        if (diff != 0)
+        {
+                puts("nope, incorrect username...\n");
+                return (1);
+        }
+        puts("Enter Password: ");
+        fgets(buff, 0x64, stdin);
+        diff = verify_user_pass(buff);
+        if (diff != 0)
+        {
+                if (diff != 0)
+                {
+                        puts("nope, incorrect password...\n");
+                        return (1);
+                }
+        }
+        return (0);
 }
